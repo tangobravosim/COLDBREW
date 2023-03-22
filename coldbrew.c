f778505f45352be405bab70a6b4529b90142ea0f
Tyler Brögger <71833469+tangobravosim@users.noreply.github.com>
2023-03-22 11:33:32 +0100

    Add files via upload

--- coldbrew.c
+++ coldbrew.c
@@ -1,58 +1,91 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <locale.h>
+#include <wchar.h>
+#include <string.h>
+#include <stdbool.h>
 #include "gruvbox.h"
 
-// Set background color.
-void brewBackgroundColor(int color){
-	printf("\033[48;5;%dm", color);
-}
+struct gruvbox gruvbox;
+struct widgetVar { int w; int h; bool darkTheme; bool waitEnd; } widget;
 
-// Set font color.
-void brewForegroundColor(int color){
-	printf("\033[38;5;%dm", color);
-}
+//	wchar_t ulCorner = L'\u256D'; // Upper left corner
+//	wchar_t urCorner = L'\u256E'; // Upper right corner
+//	wchar_t llCorner = L'\u2570'; // Lower left corner
+//	wchar_t lrCorner = L'\u256F'; // Lower right corner
+//	wchar_t hLine    = L'\u2500'; // Horizontal line
+//	wchar_t vLine    = L'\u2502'; // Vertical line
 
-// Set cursor position.
-void brewCursor(int pX, int pY){
-	printf("\033[%d;%dH", pX, pY);
-}
-
-// Clears entire terminal
-void brewClear(){
-	printf("\033[2J");
+// Move cursor to x, y
+void brewCursor(int cX, int cY){
+	printf("\033[%d;%dH", cY, cX );
 }
 
 
-// Draw a matrix of characters in a specific location, width, height and font Color.
-void brewMatrix(char *string, int pX, int pY, int mX, int mY, int fgC){
+// Draw Matrix of defined character at specific position and defined size
+void brewMatrix(char *mChar, int cursorX, int cursorY, int mWidth, int mHeight){
+	brewCursor(cursorX, cursorY);
 	int x, y;
-	brewForegroundColor(fgC);
-	brewCursor(pX, pY);
-	for(y = 0; y < mY; y++){
-		printf("\033[%dG", mY);
-		for(x = 0; x < mX; x++){
-			printf("%s", string);
+	for(y = 0; y < mHeight; y++){
+		printf("\033[%dG", cursorX);
+		for(x = 0; x < mWidth; x++){
+			printf("%s", mChar);
 		}
 		printf("\n");
 	}
 }
 
+void drawWindow(){
+	
+}
+	
 
-// Draw text in a specific location with specified color.
-void brewText(char *string, int pX, int pY, int color){
-	brewCursor(pX, pY);
-	brewForegroundColor(color);
-	printf("%s", string);
+// Initialise coldbrew - 130,10 is iSH default. Independent.
+void brewInit(int wWidth, int wHeight, bool themeIsDark, bool cWait){
+	
+	setlocale(LC_ALL, "en_US.UTF-8");
+	
+	// Initialise widget variables
+	widget.w = wWidth;
+	widget.h = wHeight;
+	widget.darkTheme = themeIsDark;
+	widget.waitEnd = cWait;
+			
+			
+	// Initialise color scheme
+	if(widget.darkTheme == true){
+		gruvbox = gruvbox_dark;
+	}else{
+		gruvbox = gruvbox_light;
+	}
+	
+	// Set background, foreground color.
+	printf("\033[2J");
+	printf("\033[1;1H");
+	printf("\033[48;5;%dm", gruvbox.bg);
+	printf("\033[38;5;%dm", gruvbox.fg);
+	
+	
+	// Draw background matrix.
+	int x, y;
+	for(y = 0; y < widget.h; y++){
+		for(x = 0; x < widget.w; x++){
+			printf(" ");
+		}
+		printf("\n");
+	}
+	
 }
 
-
-// Wait for enter key press, reset terminal to default.
 void brewEnd(){
-	getchar();
-	printf("\033c");
+	printf("\033[0m");
+	printf("\033[%d;1H", widget.h+1);
+	if(widget.waitEnd){
+		getchar();
+	}
 }
 
-// MAIN FUNCTION
 int main(){
-	brewMatrix("#",2,2,2,2,gruvbox.bg)
+	brewInit(138,10,true,false);
+	brewBlock(2,2,5,5);
+	brewEnd();
 }
